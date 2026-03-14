57830210385489fc386fc075dd07f5cdbc84c0e1
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-03-13 23:06:13 -0400

    Update index.php
    added the html, syntax error with curly brackets i put in wrong php

--- index.php
+++ index.php
@@ -3,30 +3,52 @@
 //php librayr
 spl_autoload_register(function ($class) {
 
-    $class = str_replace('PokemonOOPAssignment\\', '', $class);
+    $class = str_replace('armstrong-amy-MMED-3014-OOP\\', '', $class);
 
     $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);// for mac and windows
 
-    $filepath = __DIR__ . 'includes/classes/' . $class . '.php';
+    $filepath = __DIR__ . '/includes/classes/' . $class . '.php';
 
     require_once $filepath;
 
+});
+
 //getInfo string from the pokemon.php of pokemon,type and ability
+// the objects!!!
 $gengar = new Gengar("Gengar","Ghost/Poison","Levitate");
 $dragonite = new Dragonite("Dragonite","Dragon/Flying","Inner Focus");
 $snorlax = new Snorlax("Snorlax","Normal","Thick Fat");
 $blissey = new Blissey("Blissey","Normal","Natural Cure");
 
-echo $gengar->getInfo() . "<br>";
-echo $gengar->attack() . "<br><br>";
+?>
 
-echo $dragonite->getInfo() . "<br>";
-echo $dragonite->attack() . "<br><br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MMED-3014 OOP</title>
+</head>
+<body>
+    
+    <h1>Most OP Pokemon Team</h1>
 
-echo $snorlax->getInfo() . "<br>";
-echo $snorlax->defend() . "<br><br>";
+    <?php
+    echo $gengar->getInfo() . "<br>";
+    echo $gengar->attack() . "<br><br>";
 
-echo $blissey->getInfo() . "<br>";
-echo $blissey->support();
+    echo $dragonite->getInfo() . "<br>";
+    echo $dragonite->attack() . "<br><br>";
 
-});
+    echo $snorlax->getInfo() . "<br>";
+    echo $snorlax->defend() . "<br><br>";
+
+    echo $blissey->getInfo() . "<br>";
+    echo $blissey->support();
+
+    ?>
+</body>
+</html>
+
+
+
