df668ac6ca4246d923f5f91eca07d968c103dd0b
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-03-13 21:38:34 -0400

    Update Dragonite.php
    built dragonites class

---
+++ classes/Dragonite.php
@@ -0,0 +1,12 @@
+<?php
+
+// dragonites class 
+// hes one of the offensive pokemon
+
+class Dragonite extends OffensivePokemon {
+    
+    use SpecialMoveTrait;
+    public function attack(): string{
+        return $this->name . "uses Draco Meteor!";
+    }
+}
