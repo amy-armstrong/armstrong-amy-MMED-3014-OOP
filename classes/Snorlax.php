0cb39bcef48e8f5d4209132c53a1186adffedec1
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-03-13 21:41:13 -0400

    Update Snorlax.php
    built snorlax class

---
+++ classes/Snorlax.php
@@ -0,0 +1,12 @@
+<?php
+
+// snorlax class 
+// hes one of the defensive pokemon
+
+class Snorlax extends DefensivePokemon {
+    
+    use SpecialMoveTrait;
+    public function defend(): string{
+        return $this->name . "used Body Slam!";
+    }
+}
